03734dcd17c179d0fe7b3e4985ff2f406659d4ec
Manash <manashmndl@gmail.com>
2020-06-03 05:02:48 +0600

    add app enhancement

---
+++ .vuepress/components/Loading.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="load-div" v-loading.body="loading">
+    <h1 id="title">
+      <font-awesome-icon icon="user-secret" />{{
+        title || "Writing In Progress..."
+      }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: true,
+    };
+  },
+
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style scoped>
+#load-div {
+  width: 100%;
+}
+</style>
---
+++ .vuepress/enhanceApp.js
@@ -0,0 +1,16 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUserSecret);
+
+export default ({
+  Vue, // the version of Vue being used in the VuePress app
+  options, // the options for the root Vue instance
+  router, // the router instance for the app
+  siteData, // site metadata
+  isServer, // is this enhancement applied in server-rendering or client
+}) => {
+  Vue.component("font-awesome-icon", FontAwesomeIcon);
+  // ...apply enhancements to the app
+};
