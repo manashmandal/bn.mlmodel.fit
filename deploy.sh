342b4bd6a190d8baa5272721f20a2b15fc6c3487
Manash <manashmndl@gmail.com>
2020-06-07 01:07:43 +0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,11 @@ git add -A
 
 git commit -m "deploy"
 
-ls
+
+git config --global user.email "manashmndl@gmail.com"
+git config --global user.name "manashmandal"
+
+git push -f https://manashmandal:${{ secrets.GITHUB_TOKEN }}@github.com/manashmandal/bn.mlmodel.fit.git master:gh-pages
+
 # git push -f git@github.com:manashmandal/bn.mlmodel.fit.git master:gh-pages
+
