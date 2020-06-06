ee59528082899e77d423f27863cceeb7bbea8fd3
Manash <manashmndl@gmail.com>
2020-06-07 01:11:57 +0600

    fixing things up

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,16 @@ cd .vuepress/dist
 
 echo "bn.mlmodel.fit" > CNAME
 
+
+git config --global user.email "manashmndl@gmail.com"
+git config --global user.name "manashmandal"
+
 git init
 git add -A
 
 git commit -m "deploy"
 
-
-git config --global user.email "manashmndl@gmail.com"
-git config --global user.name "manashmandal"
-
-git push -f https://manashmandal:${{ secrets.GITHUB_TOKEN }}@github.com/manashmandal/bn.mlmodel.fit.git master:gh-pages
+git push -f https://manashmandal:$1@github.com/manashmandal/bn.mlmodel.fit.git master:gh-pages
 
 # git push -f git@github.com:manashmandal/bn.mlmodel.fit.git master:gh-pages
 
