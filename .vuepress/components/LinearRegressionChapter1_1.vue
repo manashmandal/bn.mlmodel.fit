b701d07d5a739ae26a298bc69946c9710a99b243
Manash <manashmndl@gmail.com>
2020-06-10 04:44:37 +0600

    finally working

--- .vuepress/components/LinearRegressionChapter1_1.vue
+++ .vuepress/components/LinearRegressionChapter1_1.vue
@@ -7,6 +7,7 @@
       <el-table-column prop="eating" label="প্রতি মাসে কয়বার বাইরে খেতে গিয়েছেন"></el-table-column>
     </el-table>
     <button @click="randomize">Randomize</button>
+    <input @keyup="setTheta1">Set theta value</input>
     <v-chart :options="viz"></v-chart>
   </div>
 </template>
@@ -17,10 +18,19 @@ import "echarts/lib/chart/scatter";
 
 export default {
   data() {
-    this.getData();
+    
     let tableData = [];
+    let regressionData = [];
+
+    this.getData();
+
+    let theta_0 = 0;
+    let theta_1 = 0;
+
     return {
-      tableData: tableData
+      tableData: tableData,
+      regressionData: regressionData,
+      parameters: { theta_0, theta_1 }
     };
   },
 
@@ -28,14 +38,16 @@ export default {
     viz() {
       return {
         yAxis: {
-          type: "value"
+          // type: "value"
         },
-        xAxis: {
-          data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        },
+        xAxis: {},
         series: [
           {
-            data: this.tableData.map(d => d.income),
+            data: this.tableData.map(d => [d.income, d.eating]),
+            type: "scatter"
+          },
+          {
+            data: this.regressionData.map(d => [d.income, d.eating]),
             type: "line"
           }
         ]
@@ -44,24 +56,50 @@ export default {
   },
 
   methods: {
+    setTheta1(event) {
+      this.parameters.theta_1 = +event.target.value;
+      this.getRegressionLine();
+    },
+
+    getRegressionLine() {
+      this.regressionData = [];
+      for (let i = 0; i < this.tableData.length; i++){
+        this.regressionData.push({
+          income: this.tableData[i].income,
+          eating: this.tableData[i].income * this.parameters.theta_1 + this.parameters.theta_0
+        })
+      }
+
+      console.table(this.regressionData)
+      console.log({parameters: this.parameters})
+    },
+
     async getData() {
+      this.regressionData = [];
       this.tableData = [];
+
       for (let i = 0; i < 10; i++) {
         let income = Math.round(Math.random() * 100);
         let noise = await tf.randomNormal([1]).data();
         noise = Math.abs(noise[0]);
-        let eating = Math.round(income * 0.5 - noise);
+        let eating = Math.abs(Math.round(income * 0.5 - noise * 5));
+
+
         this.tableData.push({
           income,
           eating
         });
+
+        // // This is regression line
+        
       }
 
       this.tableData.sort((a, b) => {
         if (a.income < b.income) return -1;
         return a.income > b.income ? 1 : 0;
       });
-      // console.log(await tf.randomNormal([1]).data());
+
+      this.getRegressionLine();
     },
 
     randomize(event) {
